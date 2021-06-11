0f864e8eab25b4f5844e1f1bff58155c97d32afb
pkss123 <80875171+pkss123@users.noreply.github.com>
2021-06-11 21:36:47 +0900

    Add files via upload
    파이썬 5일차

---
+++ ifQ1.py
@@ -0,0 +1,8 @@
+height = int(input("키 입력 : "))
+age = int(input("나이 입력 : "))
+
+if height >= 140 and age >= 8:
+    print("놀이기구에 탑승할 수 있습니다")
+else:
+    print("놀이기구에 탑승할 수 없습니다")
+print("오늘 하루 즐거운 시간되세요")
